d06af37e1f8976e07bf5d3c3daf1b2b8d3cf15c5
edu-msk3-407t <test@test.com>
2020-09-22 22:03:19 +0300

    При нажатии кнопки с цифрой теперь она добавляется на дисплей

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void numberClick(View view){
+        Button button = (Button) view;
+        if (textView.getText().toString().equals("0")){
+            textView.setText(button.getText().toString());
+        } else {
+            textView.setText(textView.getText().toString() + button.getText().toString());
+        }
     }
 }
